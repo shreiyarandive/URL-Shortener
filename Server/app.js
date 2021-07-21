78597f5157856efa66cac720da71ce4ce1661911
shreiyarandive <shreiyarandive1@gmail.com>
2021-07-21 17:35:42 +0530

    minor changes

--- Server/app.js
+++ Server/app.js
@@ -1,71 +1,70 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const ShortUrl = require('./models/shortUrl');
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 mongoose.Promise = global.Promise;
 
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
-app.get('/getUrls', async (req, res, next) => {
-    try {
-        const url = await ShortUrl.find();
-        if (url.length < 1) {
-            res.status(401).json({
-                error: "No Data"
-            });
-        } else {
-            res.status(200).json(url);
-        }
-    } catch (err) {
-        console.log('err', err);
+app.get("/getUrls", async (req, res, next) => {
+  try {
+    const url = await ShortUrl.find();
+    if (url.length < 1) {
+      res.status(401).json({
+        error: "No Data",
+      });
+    } else {
+      res.status(200).json(url);
     }
-})
+  } catch (err) {
+    console.log("err", err);
+  }
+});
 
-app.get('/:shortUrl', async (req, res) => {
-
-    try {
-        const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-        if (shortUrl == null) {
-            res.status(404).json({
-                error: "No data"
-            })
-        }
-        shortUrl.clicks++;
-        shortUrl.save();
-        res.redirect(shortUrl.full);
+app.get("/:shortUrl", async (req, res) => {
+  try {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+    if (shortUrl == null) {
+      res.status(404).json({
+        error: "No data",
+      });
     }
-    catch (err) {
-        console.log('err', err);
-    }
-})
-app.post('/shortUrls', async (req, res) => {
-
-    try {
-        const shortUrl = new ShortUrl({
-            full: req.body.url
-        });
-        await shortUrl.save();
-        res.status(200).json({
-            message: "Done"
-        });
-    } catch (err) {
-        console.log('err', err);
-    }
-})
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.full);
+  } catch (err) {
+    console.log("err", err);
+  }
+});
+app.post("/shortUrls", async (req, res) => {
+  try {
+    const shortUrl = new ShortUrl({
+      full: req.body.url,
+    });
+    await shortUrl.save();
+    res.status(200).json({
+      message: "Done",
+    });
+  } catch (err) {
+    console.log("err", err);
+  }
+});
 module.exports = app;
--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,6 @@
-const app = require('./app')
+const app = require("./app");
 const port = 5000;
 
-
 app.listen(port, () => {
-    console.log('Server started at ', port);
-})
+  console.log("Server started at ", port);
+});
